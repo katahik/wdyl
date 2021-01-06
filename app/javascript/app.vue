d1ff6a502d6e6f5296aa03501afd7e55584d41f1
katahik <koutakatahira0@gmail.com>
2021-01-06 09:13:12 +0900

    apiでjsonwo を使ってランダムな画面を2枚表示させた

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,14 +1,8 @@
 <template>
   <div id="app">
-    <div>
-      <input v-model="name" placeholder="name">
-      <input v-model="period_start" placeholder="period_start">
-      <input v-model="period_end" placeholder="period_end">
-      <button @click="addCompetition">addCompetition</button>
-    </div>
     <ul>
-      <li v-for="competition in competitions" :key="competition.id">
-        {{competition.name}}:{{competition.period_start}}〜{{competition.period_end}}
+      <li v-for="item in items" :key="item.id">
+        {{item.name}}
       </li>
     </ul>
   </div>
@@ -20,36 +14,23 @@ import axios from 'axios';
 export default {
   data: function () {
     return {
-      competitions: "competitions",
-      name: '',
-      period_start: '',
-      period_end: '',
+      items:"items"
     }
   },
   mounted() {
-    this.setCompetition();
+    this.setItems();
   },
   methods:{
     // setCompetitionsメソッドを定義
-    setCompetition: function(){
+    setItems: function(){
       // GETメソッドでapi/competitionsとしたとき
-      axios.get('/api/competitions')
+      axios.get('/api/items')
           // 成功したら、
       .then(response=>(
           // Axiosで呼び出したAPIの情報をcompetitionsに格納
-          this.competitions = response.data
+          this.items = response.data
       ))
     },
-    addCompetition: function (){
-      axios.post('/api/competitions',{
-        title:this.name,
-        period_start:this.period_start,
-        period_end:this.period_end,
-      })
-      .then(response=>(
-          this.setCompetition()
-      ));
-    }
   }
 
 }
