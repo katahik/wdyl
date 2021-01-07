a42e48862fe7f5bbae403f05ccc678ddcd1233a2
katahik <koutakatahira0@gmail.com>
2021-01-07 22:56:21 +0900

    コメントの挿入

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -13,12 +13,15 @@ class Api::ItemsController < ApplicationController
         @selected_item.points += 1
         # 保存
         @selected_item.save
-        # 今日開催中の大会のitemsをRANDOMメソッドで並び替え、その中の2つを@random_itemsに入れて、テンプレートへ渡す
+
         @random_items = fetch_items
+        # JSON形式で出力する
         render json: @random_items
     end
 
     private
+    # fetchとは呼び出すこと
+    # 今現在、開催中の大会から、2つのitemの呼び出す
     def fetch_items
         in_session_competition =
             Competition.find_by(period_start: -Float::INFINITY..Date.today, period_end: Date.today..Float::INFINITY)
