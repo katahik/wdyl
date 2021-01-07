fe703618e206a92d998e8c7978ff15bdf92b0714
katahik <koutakatahira0@gmail.com>
2021-01-07 22:40:41 +0900

    画面遷移せずに画像の切り替え完了

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -2,13 +2,7 @@ class Api::ItemsController < ApplicationController
     require "date"
 
     def index
-        # Competitionモデルの中で、period_startが今日以前で、なおかつ、
-        # period_endが今日以降をin_session_competitionに格納する
-        in_session_competition =
-            Competition.find_by(period_start: -Float::INFINITY..Date.today, period_end: Date.today..Float::INFINITY)
-
-        # 今日開催中の大会のitemsをRANDOMメソッドで並び替え、その中の2つを@random_itemsに入れて、テンプレートへ渡す
-        @random_items = in_session_competition.items.order("RANDOM()").limit(2)
+        @random_items = fetch_items
     end
 
     def update
@@ -20,8 +14,16 @@ class Api::ItemsController < ApplicationController
         # 保存
         @selected_item.save
         # 今日開催中の大会のitemsをRANDOMメソッドで並び替え、その中の2つを@random_itemsに入れて、テンプレートへ渡す
-        # @random_items = in_session_competition.items.order("RANDOM()").limit(2)
-        redirect_to("/")
+        @random_items = fetch_items
+        render json: @random_items
     end
 
+    private
+    def fetch_items
+        in_session_competition =
+            Competition.find_by(period_start: -Float::INFINITY..Date.today, period_end: Date.today..Float::INFINITY)
+
+        # 今日開催中の大会のitemsをRANDOMメソッドで並び替え、その中の2つを@random_itemsに入れて、テンプレートへ渡す
+        in_session_competition.items.order("RANDOM()").limit(2)
+    end
 end
