d39a31e56c28f302886fd1dd69c9bd4542da21da
katahik <koutakatahira0@gmail.com>
2021-01-08 06:31:54 +0900

    insert comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   root 'home#index'
-  resources :competitions,only:[:index,:show,:create]
+  resources :competitions,only:[:index,:show]
   namespace :api,format:'json' do
-
       resources :items,only:[:index,:update]
   end
 end
