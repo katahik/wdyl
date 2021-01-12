0a824f036407b0aec9ace980bacde2d9012e639b
katahik <koutakatahira@gmail.com>
2021-01-13 06:10:01 +0900

    usernameをテーブルに含めた

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_12_100306) do
+ActiveRecord::Schema.define(version: 2021_01_12_210809) do
 
   create_table "competitions", force: :cascade do |t|
     t.string "name"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2021_01_12_100306) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
