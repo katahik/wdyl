5749699af06e6f74890e79cb383e3dde88dba78e
katahik <koutakatahira@gmail.com>
2021-01-21 05:03:47 +0900

    タイトルで検索機能完成

--- app/controllers/competitions_controller.rb
+++ app/controllers/competitions_controller.rb
@@ -12,14 +12,16 @@ class CompetitionsController < ApplicationController
         # will_paginateのgemを使ってページネーションを作成
         # params[:page]はwill_paginateによって自動で生成される
         # 今回は、オプションで1ページあたりの項目数を5に指定（デフォルトは30）
-        @competitions = past_competitions.paginate(page: params[:page], per_page: 5).order(period_start: "DESC")
-
+        # searchメソッドでユーザーが検索したものがparamsの中に入っている.searchメソッドはcompetition.rbに定義する
+        @competitions = past_competitions
+                            .paginate(page: params[:page], per_page: 5)
+                            .order(period_start: "DESC")
+                            .search(params[:search])
         # chosenitemsとitemsテーブルをひっつけて、competition_idで分けてから,また、item_idで分けて、item_idをカウントした
         # item_idの個数がわかったから、それを元にランク付けを行った
         # RANK() OVER (PARTITION BY competition_id ORDER BY COUNT DESC) AS rank
         #  → competition_idごとに振り分けて、COUNTの数によって順位付けして、rankという列を作り、そこに表示
         # 最後に、rank=1、competitionでくくってrankが1のものでcompetition_idでのみを抽出する
-
         winners_in_index =
             "SELECT *
                 FROM (
@@ -43,6 +45,8 @@ class CompetitionsController < ApplicationController
         # select_all -> セレクト文の結果取得
         @winners_in_index= ActiveRecord::Base.connection.select_all(winners_in_index).to_ary
     end
+
+
 
     # GET /competition/:id
     def show
--- app/models/competition.rb
+++ app/models/competition.rb
@@ -1,3 +1,14 @@
 class Competition < ApplicationRecord
     has_many :items
+
+    # controllerでsearchメソッドを使うためここで定義
+    # selfはCompetition.引数にはユーザーが入力した検索文字がparamsの中に入って渡ってくる
+    def self.search(search)
+        if search
+            Competition.where(['name LIKE ?', "%#{search}%"])
+        else
+            Competition.all
+        end
+    end
+
 end
